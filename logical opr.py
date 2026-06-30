88f40d74680fbe839b94d0f905cfa59459333dd8
lazy-codes <demonicscalan@gmail.com>
2026-06-30 23:38:02 +0530

    Login logic (30-6-26)

---
+++ logical opr.py
@@ -0,0 +1,10 @@
+#logical operator(1)
+
+username = input("Enter your username here :)
+password = int(input("Enter your password here :"))
+
+if username == "lazy" and password == 2008:
+	print("Access granted!!")
+	
+else:
+	print("Invalid username or password.Try again")
