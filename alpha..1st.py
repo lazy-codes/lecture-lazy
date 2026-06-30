8286099452abc118450e119287bc850353e4feaa
lazy-codes <demonicscalan@gmail.com>
2026-06-30 23:21:28 +0530

    Add files via upload

---
+++ alpha..1st.py
@@ -0,0 +1,13 @@
+#comparison operator (2)
+
+str_1 = input("Enter your string ':")
+str_2 = input("Enter your string ':")
+
+if str_1 > str_2:
+	print(f" {str_1} comes first alphabetically"")
+	
+elif str_1 < str_2:
+	print(f" {str_2} comes first alphabetically"")
+	
+else:
+	print("Both strings are same.")
