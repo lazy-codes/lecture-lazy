8547933a495c167de02842d37a30ac2137e1d11c
lazy-codes <demonicscalan@gmail.com>
2026-07-01 17:56:32 +0530

    Add files via upload

---
+++ pnz.py
@@ -0,0 +1,49 @@
+#Positive,Negative or zero
+'''
+number = int(input("Enter a number here :"))
+
+if number > 0:
+    print(f"{number} is positive number.")
+
+elif number < 0:
+    print(f"{number} is negative number.")
+
+else:
+    print("Zero.")
+'''
+
+# grade calculator
+'''
+percentage = float(input("Enter your percentage:"))
+
+if percentage >= 90:
+    print("You've got A grade!!!")
+
+elif percentage >= 75:
+    print("You've got B grade!!!")
+
+elif percentage >= 60:
+    print("You've got C grade!!!")
+
+else:
+    print("You've got D grade!!!")
+'''
+
+#table
+'''
+number = int(input("Multiplication table of:"))
+
+i = 1
+while i<= 10:
+    print(f"{number}x{i}={number * i}")
+    i+=1
+'''
+
+#
+
+
+
+
+
+
+
